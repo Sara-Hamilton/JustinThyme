6efccd0a3ecd236457c0b43bd050be3ee9d69bbe
JasonOnes <jasonr.jones14@gmail.com>
2017-10-22 10:40:16 -0700

    demo time

--- src/main/java/com/JustinThyme/justinthymer/controllers/TwilioReminder/TwillSend.java
+++ src/main/java/com/JustinThyme/justinthymer/controllers/TwilioReminder/TwillSend.java
@@ -8,14 +8,10 @@ import com.twilio.type.PhoneNumber;
 public class TwillSend {
 
     // Setup your own account for personal SID, TOKEN, and phone number
-    //public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-    //public static final String  AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String  AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
-
-    public static final String ACCOUNT_SID = "AC7e4d906c4961839cc535c19c394a67fb";
-
-    public static final String  AUTH_TOKEN = "09f34321dde531039ce8d5fdf43c8053";
 
 
 
@@ -26,9 +22,9 @@ public class TwillSend {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message twill_message = Message.creator(new PhoneNumber (number),//number to (receiving)
-                //new PhoneNumber("XXXXXXXXXXX"),//number from (this will be your Twillio number)
+                new PhoneNumber("XXXXXXXXXXX"),//number from (this will be your Twillio number)
 
-                new PhoneNumber("5037838539"),
+
 
 
                 message).create();//message from form and passed via model
